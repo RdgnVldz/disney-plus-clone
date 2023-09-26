6305290681ed8406804b08a8f076eb2b6d75993e
Rodgen A. Valdez <97655604+RdgnVldz@users.noreply.github.com>
2023-09-26 13:57:42 +0800

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,10 +6,10 @@ const Login = (props) => {
 		<Container>
 			<Content>
 				<CTA>
-					<CTALogoOne src="/images/cta-logo-one.svg" alt="CTA Logo One" />
+					<CTALogoOne src="./images/cta-logo-one.svg" alt="CTA Logo One" />
 					<SignUp>GET ALL THERE</SignUp>
 					<Description>Get Premier Access to Raya and the Last Dragon for an additional fee with a Disney+ subscription. As of 08/17/2023, the price of Disney+ and The Disney Bundle will increase by $1.</Description>
-					<CTALogoTwo src="/images/cta-logo-two.png" alt="CTA Logo Two" />
+					<CTALogoTwo src="./images/cta-logo-two.png" alt="CTA Logo Two" />
 				</CTA>
 				<BgImage />
 			</Content>
@@ -44,7 +44,7 @@ const BgImage = styled.div`
 	background-position: top;
 	background-size: cover;
 	background-repeat: no-repeat;
-	background-image: url("/images/login-background.jpg");
+	background-image: url("./images/login-background.jpg");
 	position: absolute;
 	top: 0;
 	right: 0;
